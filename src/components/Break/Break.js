a228a40b0ec92b44504b2f78cac6e22514520a8a
Masud-Ahmed35 <masud.cse.uap@gmail.com>
2022-09-29 20:29:47 +0600

    responsiveness of breakTime Button

--- src/components/Break/Break.js
+++ src/components/Break/Break.js
@@ -4,27 +4,27 @@ const Break = ({ breakTime }) => {
     return (
         <div className='mt-7'>
             <h2 className='text-xl font-semibold'>Add a Break</h2>
-            <div className='bg-stone-200 mt-3 p-2 flex  justify-between rounded-lg'>
+            <div className='bg-stone-200 mt-3 p-2 flex  justify-evenly rounded-lg'>
                 <button onClick={() => {
                     breakTime(10);
                     localStorage.setItem('breakTime', 10);
-                }} className="btn btn-outline btn-secondary rounded-full">10s</button>
+                }} className="btn btn-outline btn-secondary rounded-full w-11">10s</button>
                 <button onClick={() => {
                     breakTime(20);
                     localStorage.setItem('breakTime', 20);
-                }} className="btn btn-outline btn-secondary rounded-full">20s</button>
+                }} className="btn btn-outline btn-secondary rounded-full w-11">20s</button>
                 <button onClick={() => {
                     breakTime(30);
                     localStorage.setItem('breakTime', 30);
-                }} className="btn btn-outline btn-secondary rounded-full">30s</button>
+                }} className="btn btn-outline btn-secondary rounded-full w-11">30s</button>
                 <button onClick={() => {
                     breakTime(40);
                     localStorage.setItem('breakTime', 40);
-                }} className="btn btn-outline btn-secondary rounded-full">40s</button>
+                }} className="btn btn-outline btn-secondary rounded-full w-11">40s</button>
                 <button onClick={() => {
                     breakTime(50);
                     localStorage.setItem('breakTime', 50);
-                }} className="btn btn-outline btn-secondary rounded-full">50s</button>
+                }} className="btn btn-outline btn-secondary rounded-full w-11">50s</button>
             </div>
         </div>
     );
