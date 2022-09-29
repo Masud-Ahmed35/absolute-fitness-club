b1ca7640f61d229a646d0e87ebc289f5bf216e12
Masud-Ahmed35 <masud.cse.uap@gmail.com>
2022-09-29 06:22:31 +0600

    complete break component

--- src/Info/Info.js
+++ src/Info/Info.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Break from '../components/Break/Break';
 import PersonalInfo from '../PersonalInfo/PersonalInfo';
 
 const Info = () => {
     return (
         <div className='ml-5'>
             <PersonalInfo></PersonalInfo>
+            <Break></Break>
         </div>
     );
 };
---
+++ src/components/Break/Break.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Break = () => {
+    return (
+        <div className='mt-5'>
+            <h2 className='text-xl font-semibold'>Add a Break</h2>
+            <div className='bg-stone-200 mt-3 p-2 flex  justify-between rounded-lg'>
+                <button className="btn btn-outline btn-secondary rounded-full">10s</button>
+                <button className="btn btn-outline btn-secondary rounded-full">20s</button>
+                <button className="btn btn-outline btn-secondary rounded-full">30s</button>
+                <button className="btn btn-outline btn-secondary rounded-full">40s</button>
+                <button className="btn btn-outline btn-secondary rounded-full">50s</button>
+            </div>
+        </div>
+    );
+};
+
+export default Break;
